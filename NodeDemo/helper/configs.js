10ef5024aa4d20f3ed281d2f4b456e46a56405e5
Lê Anh Tuấn <leanhtuan.svhutech@gmail.com>
2024-01-06 22:39:51 +0700

    first commit

---
+++ NodeDemo/helper/configs.js
@@ -0,0 +1,11 @@
+module.exports= {
+    SECRET_KEY:"heheheehe",
+    EXP: '20d',
+    cookieExp: 2,
+    
+    MAIL_HOST:'smtp.gmail.com',
+    MAIL_PORT: 465,
+    MAIL_USER: '',
+    MAIL_PASS:'',
+
+}
